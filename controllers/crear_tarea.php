44a16b372ff3443b1f6318daf4491280bd7c164f
Alex Salas Galán <galsaan@macbook-air-de-alex.home>
2026-05-28 12:14:28 +0200

    feat: crear controlador para guardar tareas

---
+++ controllers/crear_tarea.php
@@ -0,0 +1,36 @@
+<?php
+
+header("Content-Type: application/json");
+
+require '../config/db.php';
+require '../models/Tarea.php';
+
+$db_class = new Database();
+
+$db = $db_class->getConnection();
+
+$tarea = new Tarea($db);
+
+if(!isset($_POST['titulo'])){
+    http_response_code(400);
+    echo json_encode(['status'=>'error','mensaje'=>'Faltan argumentos.']);
+    exit();
+}
+
+$titulo = $_POST['titulo'];
+$descripcion = $_POST['descripcion'] ?? null;
+$fecha_limite = $_POST['fecha_limite'] ?? null;
+
+try{
+
+   if($tarea->crear($titulo, $descripcion, $fecha_limite)){
+        http_response_code(201);
+        echo json_encode(['status'=>'ok','mensaje'=>'Tarea guardada.']);
+        exit();
+    }
+
+} catch(\PDOException $e){
+    http_response_code(500);
+    echo json_encode(['status'=>'error','mensaje'=>'Error al crear la Tarea.']);
+    exit();
+}
